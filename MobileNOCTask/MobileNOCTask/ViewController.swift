6395c667f50d5bb64cb2c420154fbca328f0ceaf
Mohammed Elnaggar <moelnaggar14@gmail.com>
2019-01-04 17:08:29 +0200

    start project
    - setup project

---
+++ MobileNOCTask/MobileNOCTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileNOCTask
+//
+//  Created by Mohammed Elnaggar on 1/4/19.
+//  Copyright © 2019 Mohammed Elnaggar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
